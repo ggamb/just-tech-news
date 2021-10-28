28909948b78a5ab8a3651dbc999e87ff7ce5ee8e
ggamb <glenngambardella372@gmail.com>
2021-10-27 21:22:20 -0400

    fixing upvote

--- models/Post.js
+++ models/Post.js
@@ -1,45 +1,43 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const Vote = require('./Vote');
 
-// create our Post model
-class Post extends Model {
-    
-}
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
 
-// create fields/columns for Post model
-Post.init(
-    {
-        id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-        },
-        title: {
-        type: DataTypes.STRING,
-        allowNull: false
-        },
-        post_url: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            isURL: true
-        }
-        },
-        user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: 'user',
-            key: 'id'
-        }
-        }
-    },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'post'
-    }
-);
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
-module.exports = Post;
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
